276eec439ae530e99f2df753e8fa674aa2d48548
arkk200 <arkk20000@gmail.com>
2022-09-28 17:57:11 +0900

    Fixed Model path

--- react-three-fiber-intro/src/components/Cars.jsx
+++ react-three-fiber-intro/src/components/Cars.jsx
@@ -35,7 +35,7 @@ const Cars = ({ }) => {
             </Dragable>
             <group rotation={[0, Math.PI, 0]}>
                 <Model 
-                    path="mech_drone/scene.gltf" 
+                    path="/mech_drone/scene.gltf" 
                     scale={new Array(3).fill(5)}
                 />
             </group>
